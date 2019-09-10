8dffadf38d761836e23c283d4319706053bec313
Sunguk-hwang <hsu940103@gmail.com>
2019-09-10 17:00:53 +0900

    chap01

---
+++ chap01/src/sec03/exam03/Board.java
@@ -0,0 +1,13 @@
+package sec03.exam03;
+
+public class Board{
+	  public static void main(String args[]){
+	    System.out.println("\n");
+	    System.out.println("--------------------------------------");
+	    System.out.println("\n");
+	    System.out.println("1.목록  2.생성  3.보기  4.삭제  5.종료");
+	    System.out.println("\n");
+	    System.out.println("--------------------------------------");
+	    System.out.println("\n");
+	  }
+	}
